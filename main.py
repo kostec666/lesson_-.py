cab10104bdf7286b5099277b9646432b44d90294
kostec666 <ananinkostyan@gmail.com>
2024-12-24 17:59:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+team1_num = 5
+team2_num = 6
+score_1 = 40
+score_2 = 42
+team1_time = 1552.512
+team2_time = 2153.31451
+tasks_total = 82
+time_avg = 45.2
+
+if score_1 > score_2 or (score_1 == score_2 and team1_time < team2_time):
+    challenge_result = 'Победа команды Мастера кода!'
+elif score_1 < score_2 or (score_1 == score_2 and team1_time > team2_time):
+    challenge_result = 'Победа команды Волшебники данных!'
+else:
+    challenge_result = 'Ничья!'
+
+formatted_string_1 = "В команде Мастера кода участников: %d !" % team1_num
+print(formatted_string_1)
+
+formatted_string_2 = "Итого сегодня в командах участников: %d и %d !" % (team1_num, team2_num)
+print(formatted_string_2)
+
+formatted_string_3 = "Команда Волшебники данных решила задач: {} !".format(score_2)
+print(formatted_string_3)
+
+formatted_string_4 = "Волшебники данных решили задачи за {:.1f} с !".format(team2_time)
+print(formatted_string_4)
+
+formatted_string_5 = f"Команды решили {score_1} и {score_2} задач."
+print(formatted_string_5)
+
+formatted_string_6 = f"Результат битвы: {challenge_result}"
+print(formatted_string_6)
+
+formatted_string_7 = f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg:.1f} секунды на задачу!."
+print(formatted_string_7)
